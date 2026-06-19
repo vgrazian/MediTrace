89e67b87c006c72a3db00e9f7b3cca9118e6e9f6
Valerio Graziani <valerio.graziani@it.ibm.com>
2026-06-19 17:44:04 +0200

    fix: ripristina customizzazioni perse dal checkout main
    - Nome/Cognome order fix in TerapieView, MovimentiView, auditLog, reporting, promemoria
    - FarmaciView: datalist autocomplete per nomeFarmaco
    - FarmaciView: auto-switch a pannello batch dopo creazione farmaco
    - FarmaciView: padding ridotto su card e deep-panel
    - ValidatedInput: nuovo prop 'list' per supporto datalist

--- assets/confirmations-CVI7co8P.js
+++ assets/confirmations-CVI7co8P.js
@@ -1,4 +1,4 @@
-import{J as c}from"./index-B-8MdPKs.js";async function a(e){const{title:n,message:i,consequences:t=[],confirmText:s="Conferma",cancelText:m="Annulla",itemName:l,itemType:o}=e;let r=i;return l&&o&&(r=`${i}
+import{J as c}from"./index-BlOgddRf.js";async function a(e){const{title:n,message:i,consequences:t=[],confirmText:s="Conferma",cancelText:m="Annulla",itemName:l,itemType:o}=e;let r=i;return l&&o&&(r=`${i}
 
 ${o}: ${l}`),t.length>0&&(r+=`
 
