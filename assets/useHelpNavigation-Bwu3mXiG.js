d4f0c4e30bf5268ed082f102afe354e23884af01
Valerio Graziani <valerio.graziani@it.ibm.com>
2026-06-20 14:58:12 +0200

    fix: ripara CSS rotto .residenza-badge dopo rimozione sync-indicator

---
+++ assets/useHelpNavigation-Bwu3mXiG.js
@@ -0,0 +1,1 @@
+import{e as a,f as n}from"./index-DV43PmG2.js";function p(){const t=a(),u=n();function e(o){const r=`#${o}`;t.push({path:"/manuale",hash:r,query:{from:u.fullPath,helpFrom:o}})}return{goToHelpSection:e}}export{p as u};
