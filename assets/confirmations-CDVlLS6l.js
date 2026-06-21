9e905894261e3dc080daca380e7981eb55778b63
Valerio Graziani <valerio.graziani@it.ibm.com>
2026-06-21 12:12:57 +0200

    fix: update PWA manifest theme_color to #1a2e4f for Firefox + cross-browser support

--- assets/confirmations-CDVlLS6l.js
+++ assets/confirmations-CDVlLS6l.js
@@ -1,4 +1,4 @@
-import{J as c}from"./index-B9l3RTtz.js";async function a(e){const{title:n,message:i,consequences:t=[],confirmText:s="Conferma",cancelText:m="Annulla",itemName:l,itemType:o}=e;let r=i;return l&&o&&(r=`${i}
+import{J as c}from"./index-DtDjheV2.js";async function a(e){const{title:n,message:i,consequences:t=[],confirmText:s="Conferma",cancelText:m="Annulla",itemName:l,itemType:o}=e;let r=i;return l&&o&&(r=`${i}
 
 ${o}: ${l}`),t.length>0&&(r+=`
 
