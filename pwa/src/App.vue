8ec0f8187e77dd54ef523c8ea684d0da95d28d90
Valerio Graziani <valerio.graziani@it.ibm.com>
2026-07-12 09:34:43 +0200

    fix: use logo-mark.png only, remove logo-meditrace.png with blue bands

--- pwa/src/App.vue
+++ pwa/src/App.vue
@@ -181,7 +181,7 @@ async function handleRegister() {
 
       <div v-else class="login-screen">
         <div class="login-logo">
-          <img :src="logoSrc" alt="MediTrace" width="220" height="120" />
+          <img :src="logoSrc" alt="MediTrace" width="110" height="110" />
         </div>
         <h1>MediTrace</h1>
         <p>Accesso con utenza e password</p>
